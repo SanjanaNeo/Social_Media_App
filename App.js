14b7f7a93f0ea1815882f9a94db4bc6f67066cd9
sanjana <sanjana.advani@neosoftmail.com>
2023-04-03 16:03:54 +0530

    Basic screens ready with navigation

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export class App extends Component {
+
+  render() {
+    const Stack = createNativeStackNavigator();
+    return (
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="OnboardingScreen"
+          component={OnboardingScreen}
+        />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class OnboardingScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Onboarding </Text>
+      </View>
+    )
+  }
+}
+
+export default OnboardingScreen
