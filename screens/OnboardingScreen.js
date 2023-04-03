d815f8a21e6321d9209e23a5ca69531117b97a7d
sanjana <sanjana.advani@neosoftmail.com>
2023-04-03 16:42:04 +0530

    Onboarding component added

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image} from 'react-native'
+import Onboarding from 'react-native-onboarding-swiper';
 
 export class OnboardingScreen extends Component {
   render() {
     return (
       <View>
-        <Text> Onboarding </Text>
+        <Onboarding
+  pages={[
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('../assets/onboarding-img1.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    },
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('../assets/onboarding-img2.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    },
+    
+  ]}
+/>
+
       </View>
     )
   }
